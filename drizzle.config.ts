546c6c5ac7eabed79688a305c805d7198c84c898
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2023-08-12 00:28:18 +0100

    chore: app config

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "drizzle-kit";
+import { env } from "~/env";
+
+export default {
+  schema: "./app/db/schema/*",
+  out: "./app/db/migrations",
+  driver: "mysql2",
+  dbCredentials: {
+    connectionString: env.DATABASE_URL,
+  },
+} satisfies Config;
