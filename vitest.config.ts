22f94ea3558e43179970ce7a652a993ce3c6f87f
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2023-08-16 02:14:04 +0100

    test: moved file

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./app/test/setup.ts",
+    setupFiles: "./test/setup.ts",
     // exclude: [
     //   "**/node_modules/**",
     //   "**/dist/**",
