087b4151b6dea40f6600064b266ed7a56550b7cd
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2023-08-17 03:06:15 +0100

    fix: fix schema

--- app/features/projects/shared.ts
+++ app/features/projects/shared.ts
@@ -5,15 +5,27 @@ export const createProjectSchema = z.object({
     .string({ required_error: "Name is required" })
     .min(2, "Name should be more than 2 characters"),
   description: z.string().nullable(),
-  teamId: z.number({ required_error: "Team Id is required" }),
+  teamId: z
+    .union([
+      z.number({ required_error: "Team Id is required" }),
+      z
+        .string({ required_error: "Team Id is requred" })
+        .min(1, "Team Id is required"),
+    ])
+    .transform((v) => Number(v)),
 });
 
 export type ICreateProject = z.infer<typeof createProjectSchema>;
 
 export const editProjectSchema = createProjectSchema.extend({
   id: z
-    .string({ required_error: "Project Id is requred" })
-    .min(1, "Project Id is required"),
+    .union([
+      z.number({ required_error: "Project Id is required" }),
+      z
+        .string({ required_error: "Project Id is requred" })
+        .min(1, "Project Id is required"),
+    ])
+    .transform((v) => Number(v)),
 });
 
 export type IEditProject = z.infer<typeof editProjectSchema>;
