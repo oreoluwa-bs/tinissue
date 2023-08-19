56feba2243e67e4a412cf8146cfe5d90580de1a6
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2023-08-19 20:05:24 +0100

    chore: migrate folders to flat folders

--- app/routes/_auth.logout.tsx
+++
@@ -1,9 +0,0 @@
-import type { ActionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-import { logout } from "~/features/auth";
-
-export const action = async ({ request }: ActionArgs) => {
-  return logout(request);
-};
-
-export const loader = async () => redirect("/");
