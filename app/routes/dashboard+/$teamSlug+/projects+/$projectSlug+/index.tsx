880d9aed698287f3542748e2bc84afa0de2727c0
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2023-08-24 18:10:02 +0100

    fix: responsive fixes

--- app/routes/dashboard+/$teamSlug+/projects+/$projectSlug+/index.tsx
+++ app/routes/dashboard+/$teamSlug+/projects+/$projectSlug+/index.tsx
@@ -11,7 +11,7 @@ import { editProject, getProject, deleteProject } from "~/features/projects";
 import { getProjectMilestones } from "~/features/projects/milestones";
 import { statusValues } from "~/features/projects/milestones/shared";
 import { MilestoneKanbanCard } from "./components/milestone-card";
-import { Button } from "~/components/ui/button";
+import { Button, buttonVariants } from "~/components/ui/button";
 import { PlusCircleIcon } from "lucide-react";
 import { CreateMilestoneForm } from "./components/create-milestone-form";
 import { useEffect, useState } from "react";
@@ -124,6 +124,13 @@ export default function ProjectRoute() {
     <main className="py-6">
       <div className="mb-5 flex items-center justify-between">
         <h2 className="">{loaderData.project.project.name}</h2>
+        <Link
+          to={`/dashboard/${params.teamSlug}/projects/${params.projectSlug}/settings`}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          {/* <PlusCircleIcon className="mr-2 h-5 w-5" /> */}
+          Settings
+        </Link>
       </div>
 
       <section>
--- app/routes/dashboard+/$teamSlug+/projects+/$projectSlug+/settings+/_layout.tsx
+++ app/routes/dashboard+/$teamSlug+/projects+/$projectSlug+/settings+/_layout.tsx
@@ -1,16 +1,63 @@
 // Sidebar with links to the differentt settings
 
-import { NavLink, Outlet, useLocation } from "@remix-run/react";
+import { Link, NavLink, Outlet, useLocation } from "@remix-run/react";
+import { ChevronLeftIcon, MenuIcon } from "lucide-react";
+import { useState } from "react";
+import { Button, buttonVariants } from "~/components/ui/button";
 import { cn } from "~/lib/utils";
 
 export default function SettingsLayout() {
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+  const basePath = location.pathname.split("/settings")[0];
+
   return (
     <div className="grid gap-6 py-6 md:grid-cols-[200px_1fr]">
       {/* height of sticky nav */}
-      <aside className="sticky top-[105px] self-start">
-        <Sidebar />
+      <aside
+        className={cn(
+          "fixed left-0 top-[105px]  w-full  self-start transition-transform md:sticky md:px-0",
+
+          isOpen ? "translate-x-0" : "-translate-x-full",
+        )}
+      >
+        <div
+          className={cn(
+            " min-h-[calc(100vh_-_105px)] w-[80%] bg-background px-4 md:w-full",
+          )}
+        >
+          <Sidebar
+            basePath={basePath + "/settings"}
+            onNavigate={() => {
+              setIsOpen(false);
+            }}
+          />
+        </div>
+        <div
+          onClick={() => setIsOpen(!isOpen)}
+          className={cn(
+            "absolute left-0 top-0 -z-10 h-full w-full bg-background/25 blur-md transition-transform",
+          )}
+        ></div>
       </aside>
       <div>
+        {/* <Link
+          to={basePath}
+          className={buttonVariants({
+            variant: "link",
+            className: "-ml-5 mb-6 hover:no-underline md:hidden",
+          })}
+        >
+          <ChevronLeftIcon /> Back
+        </Link> */}
+        <Button
+          variant="ghost"
+          className="-ml-5 mb-6 md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <MenuIcon />
+        </Button>
+
         <Outlet />
       </div>
     </div>
@@ -34,32 +81,50 @@ const routes = [
   },
 ];
 
-function Sidebar() {
-  const location = useLocation();
-  const basePath = location.pathname.split("/settings")[0] + "/settings";
-
+function Sidebar({
+  basePath,
+  onNavigate,
+}: {
+  basePath: string;
+  onNavigate: () => void;
+}) {
   return (
-    <div className="flex flex-col items-center gap-2 py-4">
-      {routes.map((route) => {
-        const url = typeof route.url === "function" ? route.url() : route.url;
+    <div>
+      <Link
+        to={basePath.split("/settings")[0]}
+        className={buttonVariants({
+          variant: "link",
+          className: "hover:no-underline",
+        })}
+      >
+        <ChevronLeftIcon /> Back
+      </Link>
 
-        return (
-          <NavLink
-            key={route.id}
-            to={basePath + url}
-            className={({ isActive }) =>
-              cn(
-                "group inline-flex h-10 w-full items-center rounded-md bg-background px-3 py-1 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
-                isActive && "bg-accent/50",
-              )
-            }
-            end={route.isEnd}
-          >
-            {/* <route.icon strokeWidth={1.5} /> */}
-            <span className="ml-2">{route.label}</span>
-          </NavLink>
-        );
-      })}
+      <div className="flex flex-col items-center gap-2 py-4">
+        {routes.map((route) => {
+          const url = typeof route.url === "function" ? route.url() : route.url;
+
+          return (
+            <NavLink
+              key={route.id}
+              to={basePath + url}
+              className={({ isActive }) =>
+                cn(
+                  "group inline-flex h-10 w-full items-center rounded-md bg-background px-3 py-1 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
+                  isActive && "bg-accent/50",
+                )
+              }
+              onClick={() => {
+                onNavigate();
+              }}
+              end={route.isEnd}
+            >
+              {/* <route.icon strokeWidth={1.5} /> */}
+              <span className="ml-2">{route.label}</span>
+            </NavLink>
+          );
+        })}
+      </div>
     </div>
   );
 }
