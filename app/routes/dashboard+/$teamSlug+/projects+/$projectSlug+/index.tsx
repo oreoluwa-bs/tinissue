988cea9196a719a8b8aa9ead5e4f8ed6d4d478f2
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2023-08-27 19:42:47 +0100

    feat: setup drag and drop for milestone card

--- app/routes/dashboard+/$teamSlug+/projects+/$projectSlug+/index.tsx
+++ app/routes/dashboard+/$teamSlug+/projects+/$projectSlug+/index.tsx
@@ -14,7 +14,7 @@ import { MilestoneKanbanCard } from "./components/milestone-card";
 import { Button, buttonVariants } from "~/components/ui/button";
 import { PlusCircleIcon } from "lucide-react";
 import { CreateMilestoneForm } from "./components/create-milestone-form";
-import { useEffect, useState } from "react";
+import { useEffect, useId, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -25,6 +25,17 @@ import {
 import { DialogTitle } from "@radix-ui/react-dialog";
 import { useToast } from "~/components/ui/use-toast";
 import { editProjectSchema } from "~/features/projects/shared";
+import {
+  type Active,
+  DndContext,
+  DragOverlay,
+  useDraggable,
+  useDroppable,
+  useSensors,
+  PointerSensor,
+  useSensor,
+} from "@dnd-kit/core";
+import { cn } from "~/lib/utils";
 
 export async function action({ params, request }: ActionArgs) {
   const userId = await requireUserId(request);
@@ -118,7 +129,39 @@ export default function ProjectRoute() {
   const params = useParams();
   const loaderData = useLoaderData<typeof loader>();
   const newMilestone = useFetcher();
+  const fetcher = useFetcher();
+  const [isDragging, setIsDragging] = useState(false);
+  const id = useId();
+  const [active, setActvie] = useState<Active | null>(null);
   // console.log(loaderData);
+
+  const activeMilestone = loaderData.milestones.find(
+    (item) => item.milestone.id === active?.data?.current?.id,
+  );
+
+  const optimisticMilestones = [...loaderData.milestones].map((item) => {
+    if (item.milestone.id !== Number(fetcher.formData?.get("id"))) return item;
+
+    return {
+      ...item,
+      milestone: {
+        ...item.milestone,
+        status:
+          (fetcher.formData?.get(
+            "status",
+          ) as (typeof loaderData.milestones)[0]["milestone"]["status"]) ??
+          item.milestone.status,
+      },
+    };
+  });
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 8,
+      },
+    }),
+  );
 
   return (
     <main className="py-6">
@@ -134,26 +177,68 @@ export default function ProjectRoute() {
       </div>
 
       <section>
-        <div className="flex h-[75vh] flex-1 gap-4  overflow-auto">
-          {statusValues.map((stat, index, arr) => {
-            const milestones = loaderData.milestones.filter(
-              (item) => item.milestone.status === stat,
-            );
+        <DndContext
+          id={id}
+          sensors={sensors}
+          onDragStart={(event) => {
+            setActvie(event.active);
+            setIsDragging(true);
+          }}
+          onDragEnd={({ over, active }) => {
+            // setParent(over ? over.id : null);
+            if (over?.data.current && active.data.current) {
+              fetcher.submit(
+                { status: over.data.current.stat, id: active.data.current.id },
+                {
+                  method: "PATCH",
+                  action: `/dashboard/${params["teamSlug"]}/projects/${loaderData.project.project}/milestones/${active.data.current.id}/status`,
+                },
+              );
+            }
+            setIsDragging(false);
+          }}
+          onDragCancel={() => setIsDragging(false)}
+          autoScroll={true}
+        >
+          <div className="flex h-[75vh] flex-1 gap-4  overflow-auto">
+            {statusValues.map((stat, index, arr) => {
+              const milestones = optimisticMilestones.filter(
+                (item) => item.milestone.status === stat,
+              );
 
-            return (
-              <Board
-                key={stat}
-                stat={stat}
-                project={loaderData.project}
-                milestones={milestones}
-                newMilestone={newMilestone}
-                teamSlug={params["teamSlug"] as string}
-                prevStat={arr[index - 1] ?? null}
-                nextStat={arr[index + 1] ?? null}
+              return (
+                <Board
+                  key={stat}
+                  stat={stat}
+                  project={loaderData.project}
+                  milestones={milestones}
+                  // milestones={[...newMilestoneList, ...newMi]}
+                  newMilestone={newMilestone}
+                  teamSlug={params["teamSlug"] as string}
+                  prevStat={arr[index - 1] ?? null}
+                  nextStat={arr[index + 1] ?? null}
+                />
+              );
+            })}
+          </div>
+          <DragOverlay dropAnimation={null}>
+            {isDragging && activeMilestone ? (
+              <MilestoneKanbanCard
+                milestone={{
+                  ...activeMilestone.milestone,
+                  createdAt: new Date(activeMilestone.milestone.createdAt),
+                  updatedAt: activeMilestone.milestone.updatedAt
+                    ? new Date(activeMilestone.milestone.updatedAt)
+                    : null,
+                }}
+                projectSlug={loaderData.project.project.slug!}
+                teamSlug={params["teamSlug"]!}
+                assignees={activeMilestone.assignees.filter(Boolean) as any}
+                members={loaderData.project.members}
               />
-            );
-          })}
-        </div>
+            ) : null}
+          </DragOverlay>
+        </DndContext>
       </section>
     </main>
   );
@@ -166,7 +251,7 @@ function Board({
   teamSlug,
   milestones,
   prevStat,
-  nextStat,
+  nextStat, // isDragging,
 }: {
   stat: "BACKLOG" | "TODO" | "IN PROGRESS" | "DONE" | "CANCELLED";
   newMilestone: ReturnType<typeof useFetcher<any>>;
@@ -175,9 +260,16 @@ function Board({
   teamSlug: string;
   nextStat: "BACKLOG" | "TODO" | "IN PROGRESS" | "DONE" | "CANCELLED" | null;
   prevStat: "BACKLOG" | "TODO" | "IN PROGRESS" | "DONE" | "CANCELLED" | null;
+  // isDragging?: boolean;
 }) {
   const { toast } = useToast();
   const [showNewMilestoneDialog, setShowNewMilestoneDialog] = useState(false);
+  const { isOver, setNodeRef } = useDroppable({
+    id: `BOARD_${stat}`,
+    data: {
+      stat,
+    },
+  });
 
   useEffect(() => {
     if (newMilestone.state === "idle" && newMilestone.data) {
@@ -206,7 +298,7 @@ function Board({
       className="bordr flex-shrink-0 overflow-auto rounded-lg border-border bg-border/25"
       style={{ width: 350 }}
     >
-      <div className="sticky top-0 flex items-baseline justify-between bg-primary p-4  text-primary-foreground">
+      <div className="sticky top-0 z-10 flex items-baseline justify-between bg-primary p-4  text-primary-foreground">
         <h3 className="font-medium capitalize">{stat.toLowerCase()}</h3>
 
         <Dialog
@@ -241,62 +333,74 @@ function Board({
           </DialogContent>
         </Dialog>
       </div>
-      <div className="my-4  space-y-4 px-4">
+      <div
+        ref={setNodeRef}
+        className={cn(
+          "h-full space-y-4 overflow-x-hidden  rounded-[inherit] rounded-t-none px-4 py-4",
+          isOver && " ring-2 ring-inset ring-primary",
+        )}
+      >
         {milestones.map((milestone) => {
           return (
-            <Link
-              to={`/dashboard/${teamSlug}/projects/${project.project.slug}/milestones/${milestone.milestone.id}`}
+            <DraggableCard
               key={milestone.milestone.id}
-              className="block"
-              onClick={(e) => {
-                blockPropagation(e);
-              }}
+              id={milestone.milestone.id}
             >
-              <MilestoneKanbanCard
+              <Link
+                to={`/dashboard/${teamSlug}/projects/${project.project.slug}/milestones/${milestone.milestone.id}`}
                 key={milestone.milestone.id}
-                milestone={{
-                  ...milestone.milestone,
-                  createdAt: new Date(milestone.milestone.createdAt),
-                  updatedAt: milestone.milestone.updatedAt
-                    ? new Date(milestone.milestone.updatedAt)
-                    : null,
+                className={"block"}
+                onClick={(e) => {
+                  blockPropagation(e);
                 }}
-                projectSlug={project.project.slug!}
-                teamSlug={teamSlug}
-                assignees={milestone.assignees.filter(Boolean) as any}
-                members={project.members}
-                onAddAssignee={(milestoneId, assigneeId) => {
-                  newMilestone.submit(
-                    {
-                      milestoneId,
-                      "assigneesId[]": [assigneeId],
-                      errorAsToast: true,
-                    },
-                    {
-                      method: "POST",
-                      action: `/dashboard/${teamSlug}/projects/${project.project.slug}/milestones/${milestoneId}/assignees`,
-                    },
-                  );
-                }}
-                onDeleteAssignee={(milestoneId, assigneeId) => {
-                  newMilestone.submit(
-                    {
-                      milestoneId,
-                      assigneeId: assigneeId,
-                      errorAsToast: true,
-                    },
-                    {
-                      method: "DELETE",
-                      action: `/dashboard/${teamSlug}/projects/${project.project.slug}/milestones/${milestoneId}/assignees`,
-                    },
-                  );
-                }}
-                meta={{
-                  nextStat,
-                  prevStat,
-                }}
-              />
-            </Link>
+                draggable="false"
+              >
+                <MilestoneKanbanCard
+                  key={milestone.milestone.id}
+                  milestone={{
+                    ...milestone.milestone,
+                    createdAt: new Date(milestone.milestone.createdAt),
+                    updatedAt: milestone.milestone.updatedAt
+                      ? new Date(milestone.milestone.updatedAt)
+                      : null,
+                  }}
+                  projectSlug={project.project.slug!}
+                  teamSlug={teamSlug}
+                  assignees={milestone.assignees.filter(Boolean) as any}
+                  members={project.members}
+                  onAddAssignee={(milestoneId, assigneeId) => {
+                    newMilestone.submit(
+                      {
+                        milestoneId,
+                        "assigneesId[]": [assigneeId],
+                        errorAsToast: true,
+                      },
+                      {
+                        method: "POST",
+                        action: `/dashboard/${teamSlug}/projects/${project.project.slug}/milestones/${milestoneId}/assignees`,
+                      },
+                    );
+                  }}
+                  onDeleteAssignee={(milestoneId, assigneeId) => {
+                    newMilestone.submit(
+                      {
+                        milestoneId,
+                        assigneeId: assigneeId,
+                        errorAsToast: true,
+                      },
+                      {
+                        method: "DELETE",
+                        action: `/dashboard/${teamSlug}/projects/${project.project.slug}/milestones/${milestoneId}/assignees`,
+                      },
+                    );
+                  }}
+                  meta={{
+                    nextStat,
+                    prevStat,
+                  }}
+                />
+              </Link>
+            </DraggableCard>
           );
         })}
       </div>
@@ -320,4 +424,30 @@ function blockPropagation(e: React.MouseEvent<HTMLElement, MouseEvent>) {
     }
     clickedElement = clickedElement?.parentElement ?? null;
   }
+}
+
+function DraggableCard({
+  children,
+  id,
+}: {
+  children: React.ReactNode;
+  id: string | number;
+}) {
+  const { attributes, listeners, setNodeRef, isDragging } = useDraggable({
+    id: `CARD_${id}`,
+    data: {
+      id: id,
+    },
+  });
+
+  return (
+    <div
+      ref={setNodeRef}
+      {...listeners}
+      {...attributes}
+      className={cn("touch-none transition-opacity", isDragging && "opacity-0")}
+    >
+      {children}
+    </div>
+  );
 }
