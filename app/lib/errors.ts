deb7147e7a5be657bdc2fa9e1d736cc145c17822
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2023-08-31 23:05:42 +0100

    chore: error's meta

--- app/lib/errors.ts
+++ app/lib/errors.ts
@@ -1,57 +1,66 @@
+type ErrorMeta = { [key: string]: any };
 export class APIError extends Error {
   public statusCode: number;
   public status: string;
   public isOperational: boolean;
+  public meta: ErrorMeta = {};
 
-  constructor(statusCode: number, message: string) {
+  constructor(statusCode: number, message: string, meta = {}) {
     super(message ?? "Something Unexpected Occured");
 
     this.statusCode = statusCode;
     this.status = "error";
     this.isOperational = true;
+    this.meta = meta;
 
     Error.captureStackTrace(this, this.constructor);
   }
 }
 
 export class BadRequest extends APIError {
-  constructor(message = "Bad request") {
-    super(400, message);
+  constructor(message = "Bad request", meta?: ErrorMeta) {
+    super(400, message, meta);
   }
 }
 
 export class Unauthorised extends APIError {
-  constructor(message = "You don't have permission to access this.") {
-    super(401, message);
+  constructor(
+    message = "You don't have permission to access this.",
+    meta?: ErrorMeta,
+  ) {
+    super(401, message, meta);
   }
 }
 
 export class ForbiddenError extends APIError {
-  constructor(message = "Forbidden") {
-    super(403, message);
+  constructor(message = "Forbidden", meta?: ErrorMeta) {
+    super(403, message, meta);
   }
 }
 
 export class NotFound extends APIError {
-  constructor(message = "Not Found") {
-    super(404, message);
+  constructor(message = "Not Found", meta?: ErrorMeta) {
+    super(404, message, meta);
   }
 }
 
 export class MethodNotSupported extends APIError {
-  constructor(message = "This Method is not supported") {
-    super(405, message);
+  constructor(message = "This Method is not supported", meta?: ErrorMeta) {
+    super(405, message, meta);
   }
 }
 
 export class TooManyRequests extends APIError {
-  constructor(message = "Too many requests") {
-    super(429, message);
+  constructor(message = "Too many requests", meta?: ErrorMeta) {
+    super(429, message, meta);
   }
 }
 
 export class InternalServerError extends APIError {
-  constructor(message = "Uh oh! Something unexpected occured.") {
-    super(500, message);
+  constructor(
+    message = "Uh oh! Something unexpected occured.",
+    meta?: ErrorMeta,
+  ) {
+    super(500, message, meta);
   }
 }
